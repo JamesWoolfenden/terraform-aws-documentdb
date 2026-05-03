ab2432097d78d9fe43a8a203c3b7b78a2c498ab6
James Woolfenden <james.woolfenden@gmail.com>
2026-05-03 20:12:10 +0100

    fix: replace deprecated aws_subnet_ids with aws_subnets

--- example/examplea/module.documentdb.tf
+++ example/examplea/module.documentdb.tf
@@ -1,24 +1,22 @@
 module "documentdb" {
   source          = "../../"
   master_password = random_string.password.result
-  subnet_list     = data.aws_subnet_ids.examplea.ids
+  subnet_list     = data.aws_subnets.examplea.ids
   kms_key_id      = aws_kms_key.example.arn
 }
-
 //todo:this should be private
-data "aws_subnet_ids" "examplea" {
-  vpc_id = "vpc-0c33dc8cd64f408c4"
+data "aws_subnets" "examplea" {
+  filter {
+    name   = "vpc-id"
+    values = ["vpc-0c33dc8cd64f408c4"]
+  }
   tags = {
     Type = "Public"
   }
 }
-
-
 resource "random_string" "password" {
   length = 16
 }
-
-
 resource "aws_kms_key" "example" {
   enable_key_rotation = true
 }
